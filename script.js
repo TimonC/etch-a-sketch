e53d08e167adc8882f0e97e820ce09f33e1a9b9d
TCrouzen <timon.crouzen@gmail.com>
2025-01-23 17:30:07 +0100

    Added a button to let the user input the grid size

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const body = document.querySelector('body');
+button = document.createElement('button');
+button.addEventListener('click', requestGridSize);
+button.textContent = 'New grid'
+button.id = 'size-button';
+body.prepend(button);
+
 const grid = document.querySelector('#grid');
 n = 16;
 squarePercentageSize = `${Math.round(1/16*10000)/100}%`;
@@ -6,4 +13,13 @@ for (i=0;i<n*n;i++){
     square.classList.add('square');
     square.style.height =squarePercentageSize;
     grid.appendChild(square)
+}
+
+function requestGridSize(){
+    button = document.querySelector('#size-button');
+    let n = Number(prompt('Please enter how many squares you want on each side of the grid.'));
+    while(!Number.isInteger(n) || n<1 || n>100){
+        n =  Number(prompt('Invalid input. Enter an integer between 1-100, please.'));
+    }
+    return n;
 }
